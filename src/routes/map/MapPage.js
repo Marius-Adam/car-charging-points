8ec593f84cceeb33d36e4ef24811c907e17b4cf0
Marius-Adam <marius.c.adam@gmail.com>
2020-06-12 21:21:04 +0100

    Implemented Mapbox

--- src/routes/map/MapPage.js
+++ src/routes/map/MapPage.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
 
 export default function MapPage() {
+  const [viewport, setViewport] = useState({});
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  function showPosition(position) {
+    setViewport({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+      width: "100vw",
+      height: "100vh",
+      zoom: 10,
+    });
+  }
+
   return (
     <div className="map">
-      <h1>Map</h1>
+      <ReactMapGL
+        onLoad={getLocation}
+        {...viewport}
+        mapboxApiAccessToken="pk.eyJ1IjoibWNhZGFtZWsiLCJhIjoiY2tiY21lbHA0MDNkejJydXg3N3J1ZXppcSJ9.ye1zuvUop6e-tjGkns2fjQ"
+        mapStyle="mapbox://styles/mcadamek/ckbcmp4rd14dk1inzkxonhe4d"
+        onViewportChange={(viewport) => {
+          setViewport(viewport);
+        }}
+      ></ReactMapGL>
     </div>
   );
 }
