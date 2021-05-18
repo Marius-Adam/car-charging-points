b4cd532d2e2123a3095705289e36c4fd64be7791
Marius Adam <marius.adam@wearerebase.com>
2021-05-18 18:01:24 +0100

    Removes unused files

--- src/API/OpenChargerAPI.js
+++
@@ -1,9 +0,0 @@
-export function fetchChargerLocations(latitude, longitude) {
-  const queryString = `https://api.openchargemap.io/v3/poi/?output=json&distance=100&latitude=${latitude}&longitude=${longitude}`;
-  return fetch(`${queryString}`, { method: "GET" })
-    .then((r) => r.json())
-    .catch((error) => {
-      console.error(error);
-      return [];
-    });
-}
